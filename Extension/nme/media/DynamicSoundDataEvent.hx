c05294cfd1682185ac5a06cf318275d14ff7953e
Roman Kudryashov <scythianfuego@gmail.com>
2012-04-28 03:48:30 +0400

    sync

---
+++ Extension/nme/media/DynamicSoundDataEvent.hx
@@ -0,0 +1,23 @@
+/**
+ * @author Scythian
+ * 
+ * This file is a part of DynamicSound android NME extension.
+ * 
+ */
+
+package nme.media;
+
+#if (cpp || neko)
+
+class DynamicSoundDataEvent
+{
+	public var data : Array<Float>;
+	public var position : Float;
+	
+	public function new()
+	{
+		this.data = new Array<Float>();
+	}
+}
+
+#end
